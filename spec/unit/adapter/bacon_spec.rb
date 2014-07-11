c96f3962b93fbdb34de76a6c57ca08e081573fc0
Marius Rackwitz <git@mariusrackwitz.de>
2014-07-11 18:01:23 +0200

    Better specs without sub-shells

--- spec/unit/adapter/bacon_spec.rb
+++ spec/unit/adapter/bacon_spec.rb
@@ -1,42 +1,99 @@
 require 'CLIntegracon'
+require 'mocha-on-bacon'
 
 describe 'CLIntegracon::Adapter::Bacon' do
 
+  CLIntegracon.configure do
+    hook_into :bacon
+  end
+
+  # Mutes specs from output
+  def defines_specs
+    Bacon.expects(:handle_specification).yields
+  end
+
   describe '#describe_cli' do
 
-    # These specs are executed in a sub-shell, to ensure we don't get any output we don't want.
-    def evaluate_expr(expression)
-      code = <<-eos
-        CLIntegracon.configure do
-          hook_into :bacon
-        end
-
-        exit 1 unless #{expression}
-      eos
-      `bundle exec ruby -r 'bacon' -I 'lib' -r 'CLIntegracon' -e "#{code.gsub('"', '\\"')}"`
-      $?
-    end
-
     it 'is globally available' do
-      evaluate_expr("describe_cli('test') {}.is_a? Bacon::Context").should.success?
+      defines_specs.once
+      describe_cli('test') {}.should.be.an.instance_of? Bacon::Context
     end
 
     it 'is available for Bacon::Context' do
-      evaluate_expr("describe('test') { describe_cli('test') {}.is_a? Bacon::Context }").should.success?
+      defines_specs.twice
+      describe('test') do
+        describe_cli('test') {}.should.be.an.instance_of? Bacon::Context
+      end
+    end
+
+    shared 'extended context' do
+      it 'can access #describe_cli' do
+        @context.should.respond_to? :describe_cli
+      end
+
+      it 'can access to methods defined in CLIntegracon::Adapter::Bacon::Context' do
+        @context.should.respond_to? :subject
+        @context.should.respond_to? :context
+        @context.should.respond_to? :behaves_like_a
+      end
     end
 
     describe 'extends context with methods' do
-      it 'can access #subject' do
-        evaluate_expr("describe_cli('test') {}.respond_to? :subject").should.success?
+      before do
+        defines_specs.once
+        @context = describe_cli('test') {}
       end
 
-      it 'can access #context' do
-        evaluate_expr("describe_cli('test') {}.respond_to? :context").should.success?
+      behaves_like 'extended context'
+    end
+
+    describe 'extends context with methods' do
+      before do
+        defines_specs.twice
+        parent_context = self
+        describe_cli('test') do
+          @context = describe('inner') {}
+          parent_context.instance_variable_set '@context', @context
+        end
       end
 
-      it 'can access #behaves_like_a' do
-        evaluate_expr("describe_cli('test') {}.respond_to? :behaves_like_a").should.success?
+      behaves_like 'extended context'
+    end
+
+  end
+
+  describe 'Context' do
+
+    describe '#subject' do
+      # TODO
+    end
+
+    describe '#context' do
+      # TODO
+    end
+
+    describe '#behaves_like_a' do
+
+      before do
+        defines_specs.once
+        CLIntegracon::FileTreeSpecContext.any_instance.stubs(:run)
       end
+
+      it 'knows the implicit defined shared behavior' do
+        describe_cli 'git' do
+          lambda {
+            behaves_like_a 'git', dir: 'git'
+          }.should.not.raise?(NameError)
+        end
+      end
+
+      it 'executes the FileTreeSpecContext' do
+        describe_cli 'git' do
+          context.expects(:run).once
+          behaves_like_a 'git', dir: 'git'
+        end
+      end
+
     end
 
   end
