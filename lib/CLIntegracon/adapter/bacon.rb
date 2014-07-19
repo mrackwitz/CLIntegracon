7580441434fa1101887272bfa460bdee069d8fb5
Marius Rackwitz <git@mariusrackwitz.de>
2014-07-19 22:20:03 +0200

    Refactored bacon adapter

--- lib/CLIntegracon/adapter/bacon.rb
+++ lib/CLIntegracon/adapter/bacon.rb
@@ -51,57 +51,68 @@ module CLIntegracon::Adapter::Bacon
       instance_exec(*args, &Bacon::Shared[name])
     end
 
-  end
+    # Ad-hoc defines a set of shared expectations to be consumed directly by `behaves_like`.
+    # See the following example for usage:
+    #
+    #   behaves_like cli_spec('my_spec_dir') do
+    #     subject.launch()
+    #   end
+    #
+    # @note    This expects that a method `context` is defined, which is returning an
+    #          instance of {FileTreeSpecContext}.
+    #
+    # @param   [String] spec_dir
+    #          the concrete directory of the spec, see {file_spec}.
+    #
+    # @param   [String] args
+    #          the additional arguments to pass on launch to {CLIntegracon::Subject}.
+    #
+    # @return  [String]
+    #          name of the set of shared expectations
+    #
+    def cli_spec(spec_dir, args)
+      file_spec spec_dir do
+        output = subject.launch(args)
+        status = $?
 
-  # Describe a command line interface
-  # This method basically behaves like {Bacon::Context.describe}, but it provides
-  # automatically the methods #subject and #context and provides a shared context,
-  # named like the first argument subject_name with the following parameters to be
-  # passed as a hash:
-  #   * :args => the additional arguments to pass on launch to {CLIntegracon::Subject}
-  #   * :dir  => the spec dir
-  #
-  # @param  [String] subject_name
-  #         the subject name will be used as first argument to initialize
-  #         a new {CLIntegracon::Subject}, which will be accessible in the
-  #         spec by #subject.
-  #
-  # @param  [Hash<Symbol,String>] context_options
-  #         the options to configure this spec context, could be one or more of:
-  #         * :executable: the executable used to initialize {CLIntegracon::Subject}
-  #           if not given, will fallback to param {subject_name}.
-  #
-  # @param  [Block<() -> ()>] block
-  #         the block to provide further sub-specs or requirements, as
-  #         known from {Bacon::Context.describe}
-  #
-  def describe_cli(subject_name, context_options = {}, &block)
-    describe subject_name do
-      # Make Context methods available
-      # WORKAROUND: Bacon auto-inherits singleton methods to child contexts
-      # by using the runtime and won't include methods in modules included
-      # by the parent context. We have to ensure that the methods will be
-      # accessible by the child contexts by defining them as singleton methods.
-      Context.instance_methods.each do |method|
-        define_singleton_method method, Context.instance_method(method)
-      end
-
-      subject do
-        CLIntegracon::Subject.new(subject_name, context_options[:executable] || subject_name)
-      end
-
-      shared subject_name do |options|
-        args = options[:args] || ''
-        dir = options[:dir] or raise ArgumentError.new("Spec directory is missing!")
-
-        context.spec(dir).run do |spec|
-          subject.launch args do |status, output|
-            it "$ #{subject_name} #{args}" do
-              status.should.satisfy("Binary failed\n\n#{output}") do
-                status.success?
-              end
-            end
+        it "$ #{subject.name} #{args}" do
+          status.should.satisfy("Binary failed\n\n#{output}") do
+            status.success?
           end
+        end
+      end
+    end
+
+    # Ad-hoc defines a set of shared expectations to be consumed directly by `behaves_like`.
+    # See the following example for usage:
+    #
+    #   behaves_like file_spec('my_spec_dir') do
+    #     # do some changes to the current dir
+    #   end
+    #
+    # @note    This expects that a method `context` is defined, which is returning an
+    #          instance of {FileTreeSpecContext}.
+    #
+    # @param   [String] spec_dir
+    #          the concrete directory of the spec to be passed to
+    #          {FileTreeSpecContext.spec}
+    #
+    # @param   [Block<() -> ()>] block
+    #          the block which will be executed after the before state is laid out in the
+    #          temporary directory, which normally will make modifications to file system,
+    #          which will be compare to the state given in the after directory.
+    #
+    # @return  [String]
+    #          name of the set of shared expectations
+    #
+    def file_spec(spec_dir, &block)
+      raise ArgumentError.new("Spec directory is missing!") if spec_dir.nil?
+
+      shared_name = spec_dir
+
+      shared shared_name do
+        context.spec(spec_dir).run do |spec|
+          instance_eval &block
 
           spec.compare do |diff|
             it diff.expected.to_s do
@@ -135,8 +146,45 @@ module CLIntegracon::Adapter::Bacon
               end
             end
           end
-
         end
+      end
+
+      shared_name
+    end
+
+  end
+
+  # Describe a command line interface
+  # This method basically behaves like {Bacon::Context.describe}, but it provides
+  # automatically the methods #subject, #context, #cli_spec and #file_spec.
+  #
+  # @param  [String] subject_name
+  #         the subject name will be used as first argument to initialize
+  #         a new {CLIntegracon::Subject}, which will be accessible in the
+  #         spec by #subject.
+  #
+  # @param  [Hash<Symbol,String>] context_options
+  #         the options to configure this spec context, could be one or more of:
+  #         * :executable: the executable used to initialize {CLIntegracon::Subject}
+  #           if not given, will fallback to param {subject_name}.
+  #
+  # @param  [Block<() -> ()>] block
+  #         the block to provide further sub-specs or requirements, as
+  #         known from {Bacon::Context.describe}
+  #
+  def describe_cli(subject_name, context_options = {}, &block)
+    describe subject_name do
+      # Make Context methods available
+      # WORKAROUND: Bacon auto-inherits singleton methods to child contexts
+      # by using the runtime and won't include methods in modules included
+      # by the parent context. We have to ensure that the methods will be
+      # accessible by the child contexts by defining them as singleton methods.
+      Context.instance_methods.each do |method|
+        define_singleton_method method, Context.instance_method(method)
+      end
+
+      subject do
+        CLIntegracon::Subject.new(subject_name, context_options[:executable] || subject_name)
       end
 
       instance_eval &block
